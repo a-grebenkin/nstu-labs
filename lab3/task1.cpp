8c3f9c42269a691eda259f07de58ab812ad08c68
Aleksey-Grebenkin <61308932+Aleksey-Grebenkin@users.noreply.github.com>
2020-03-26 21:14:23 +0700

    Добавлены комментарии к коду

--- lab3/task1.cpp
+++ lab3/task1.cpp
@@ -13,20 +13,20 @@ struct stack
   btree **elem;
   int top;
 
-  stack(int value = 0)
+  stack(int value = 0) //конструктор
   {
     top = value;
     elem = (btree**)calloc(1, sizeof(btree));
   }
 
-  void push(btree *e)
+  void push(btree *e) //добавление узла в стек
   {
     elem[top] = e;
     top++;
     elem = (btree **)realloc(elem, sizeof(btree) * (top + 1));
   }
 
-  btree *pop()
+  btree *pop() // считываение и удаление узла из стека
   {
     if (top > 0)
     {
@@ -37,7 +37,7 @@ struct stack
       return 0;
   }
 
-  bool isEmpty()
+  bool isEmpty() // проверка стека на наличие элементов
   {
     return top ? false : true;
   }
@@ -62,57 +62,60 @@ void Ins_Btree(int val, btree **q)
     // Добавляем в правое поддерево
     Ins_Btree(val, &(*q)->right);
 }
+
+// Рекурсивная проверка равенства 2 деревьев
 bool Comp_Btre_Rec(btree *q1, btree *q2)
 {
-  if (q1 == NULL && q2 == NULL)
+  if (q1 == NULL && q2 == NULL) //если узлы одновременно отсутсвуют возвращаем истину
     return 1;
 
-  if (q1 == NULL || q2 == NULL)
+  if (q1 == NULL || q2 == NULL) //если один из узлов отсутсвует возвращаем ложь
     return 0;
 
-  if (q1->value != q2->value)
+  if (q1->value != q2->value) //если значения различаются возвращаем ложь
     return 0;
 
-  return Comp_Btre_Rec(q1->left, q2->left) * Comp_Btre_Rec(q1->right, q2->right);
+  return Comp_Btre_Rec(q1->left, q2->left) * Comp_Btre_Rec(q1->right, q2->right); //возвращаем произведение результатов сравнеия правых и левых поддеревьев первого и второго дерева
 }
 
+// Итеративная проверка равенства 2 деревьев
 bool Comp_Btre_Iter(btree *q1, btree *q2)
 {
-  stack *s1 = new stack(), *s2 = new stack();
+  stack *s1 = new stack(), *s2 = new stack(); // создаем два стека для хранения узлов правых поддеревьев
 
-  while ((q1 != NULL || !s1->isEmpty()) || (q2 != NULL || !s2->isEmpty()))
+  while (q1 != NULL || !s1->isEmpty() || q2 != NULL || !s2->isEmpty()) //  пока узел одного из деревьев существет или один из стеков не пуст
   {
-    if (s1->isEmpty() != s2->isEmpty())
+    if (s1->isEmpty() != s2->isEmpty()) // если один из стеков пуст, а другой нет возвращаем ложь
       return 0;
 
-    if (!s1->isEmpty())
+    if (!s1->isEmpty()) // если первый стек не пуст переходим к последнему записанному в него поддереву
       q1 = s1->pop();
 
-    if (!s2->isEmpty())
+    if (!s2->isEmpty()) // если второй стек не пуст переходим к последнему записанному в него поддереву
       q2 = s2->pop();
 
-    while (q1 != NULL || q2 != NULL)
+    while (q1 != NULL || q2 != NULL) // пока оба узла существуют 
     {
-      if (s1->isEmpty() != s2->isEmpty())
+      if (s1->isEmpty() != s2->isEmpty()) // если один из стеков пуст возвращаем ложь
         return 0;
 
-      if ((q1 == NULL || q2 == NULL) && !(q1 == NULL && q2 == NULL))
-        return 0; //один из узлов отсутствует
+      if ((q1 == NULL || q2 == NULL) && !(q1 == NULL && q2 == NULL)) //если один из узлов отсутствует (но не 2 одновременно) возвращаем ложь
+        return 0; 
 
-      if (q1->value != q2->value)
-        return 0; //различные значения
+      if (q1->value != q2->value) // если значения различаются возвращаем ложь
+        return 0; 
 
-      if (q1->right != NULL)
+      if (q1->right != NULL) // если узел правого поддерева первого дерева существует добавляем его в первый стек
         s1->push(q1->right);
 
-      if (q2->right != NULL)
+      if (q2->right != NULL) // если узел правого поддерева второго дерева существует добавляем его во второй стек
         s2->push(q2->right);
 
-      q1 = q1->left;
-      q2 = q2->left;
+      q1 = q1->left; // переходим к узлу левого подеерева первого дерева 
+      q2 = q2->left; // переходим к узлу левого подеерева второго дерева 
     }
   }
-  if (q1 != NULL || q2 != NULL)
+  if (q1 != NULL || q2 != NULL) // если один из узлов отсутствует возвращаем ноль
     return 0;
 
   return 1;
@@ -137,6 +140,7 @@ void Print_Btree_It(btree *p)
     }
   }
 }
+
 //Вывод содержимого дерева
 void Print_Btree(btree *p)
 {
@@ -147,6 +151,7 @@ void Print_Btree(btree *p)
   printf("%d ", p->value);
   Print_Btree(p->right);
 }
+
 int main()
 {
   int d;
