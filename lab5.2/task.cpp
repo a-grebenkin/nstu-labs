c0fed2ad21d8b62ed936fbc5fdd1f1e23f1a7611
Aleksey-Grebenkin <61308932+Aleksey-Grebenkin@users.noreply.github.com>
2020-05-06 22:26:43 +0700

    Update task.cpp

--- lab5.2/task.cpp
+++ lab5.2/task.cpp
@@ -1,32 +1,7 @@
+
 #include <iostream>
+#include "Food.h"
 using namespace std;
-class Food{
-private:
-    string name;
-    int weight;
-    int temperature;
-    int max_temperature;
-    int min_temperature;
-    int condition; //0 -переморожен 1 - перегрет 2 - нормальное
-    float heat_capacity;
-    Food(string name, int weight, int temperature,int max_temperature, int min_temperature,float heat_capacity){
-        this->name=name;
-        this->weight=weight;
-        this->max_temperature = max_temperature;
-        this->min_temperature = min_temperature;
-        this->heat_capacity = heat_capacity;
-    }
-public:
-    string GetName(){
-        return name;
-    }
-    int GetWeight(){
-        return weight;
-    }
-    int GerTemperature(){
-        return temperature;
-    }
-};
-int main(int argc, const char * argv[]) {
-    return 0;
+int main() {
+
 }
