e41e812d6952137e2472a635745bb8e9c0841204
Sacmi <rusimpleigor@gmail.com>
2020-05-07 15:50:02 +0700

    Лаба 4: подогнал под критерии

--- lab4/task1.cpp
+++ lab4/task1.cpp
@@ -21,10 +21,11 @@ struct btree
 // сравнить строки
 int compareStrings(char *stringOne, char *stringTwo)
 {
-  int length_one = strlen(stringOne);
-  int length_two = strlen(stringTwo);
-  int min_length = length_one > length_two ? length_two : length_one;
+  int length_one = strlen(stringOne),                                 // длина первой строки
+      length_two = strlen(stringTwo),                                 // длина второй строки
+      min_length = length_one > length_two ? length_two : length_one; // Минимальная длина
 
+  // сравнение по буквам
   for (size_t i = 0; i < min_length; i++)
   {
     if (stringOne[i] > stringTwo[i])
@@ -33,11 +34,13 @@ int compareStrings(char *stringOne, char *stringTwo)
       return 2;
   }
 
+  // если в слове больше букв чем в другом => оно больше
   if (length_one - min_length > 0)
     return 1;
   else if (length_two - min_length > 0)
     return 2;
 
+  // если слова равны
   return 0;
 }
 
@@ -53,7 +56,7 @@ void Ins_Btree(char *name, char *value, btree **q)
     return;
   }
 
-  compare = compareStrings((*q)->name, name);
+  compare = compareStrings((*q)->name, name); // сравнение слов
 
   if (compare == 1)
     // Добавляем в левое поддерево
@@ -66,52 +69,62 @@ void Ins_Btree(char *name, char *value, btree **q)
 // обработка слов после решетки
 btree *handleSharp(FILE *stream, btree *tree)
 {
-  char define_buffer[8], letter, *name, *value, define_str[] = "define ";
+  char define_buffer[8],        // для поиска макроса
+      letter,                   // буква
+      *name,                    // имя макроса
+      *value,                   // значение макроса
+      define_str[] = "define "; // определение макроса
   size_t position = 0;
 
+  // чтение из файла
   fread(define_buffer, sizeof(char), 7, stream);
   define_buffer[7] = '\0';
 
+  // если определение макроса не совпадает
   if (compareStrings(define_buffer, define_str))
-    return tree;
+    return tree; // возвращаем дерево
 
-  name = (char *)calloc(1, sizeof(char));
+  name = (char *)calloc(1, sizeof(char)); // место для имени макроса
 
+  // пока не конец файла
   while (!feof(stream))
   {
-    fread(&letter, sizeof(char), 1, stream);
+    fread(&letter, sizeof(char), 1, stream); // чтение буквы
 
+    // если имя макроса закончилось, переход к следующему циклу
     if (letter == ' ')
       break;
 
+    // добавление буквы в название файла
     name[position] = letter;
     position++;
-
     name = (char *)realloc(name, sizeof(char) * (position + 1));
   }
 
   name[position] = '\0';
-  value = (char *)calloc(1, sizeof(char));
+  value = (char *)calloc(1, sizeof(char)); // место для значения макроса
   position = 0;
 
+  // пока не конец файла
   while (!feof(stream))
   {
     fread(&letter, sizeof(char), 1, stream);
 
+    // если имя макроса закончилось, переход к следующему этапу
     if (letter == '\n' || letter == '\0')
       break;
 
+    // добавление буквы к значению
     value[position] = letter;
     position++;
-
     value = (char *)realloc(value, sizeof(char) * (position + 1));
   }
   value[position] = '\0';
   position = 0;
 
-  Ins_Btree(name, value, &tree);
+  Ins_Btree(name, value, &tree); // добавляем в дерево
 
-  return tree;
+  return tree; // возвращаем
 }
 
 // найти элемент в дереве
@@ -135,14 +148,19 @@ btree *getItem(btree *tree, char *name)
 // заменить все дефайны
 void replaceAllDefines(FILE *reader, FILE *writer, btree *tree)
 {
-  char letter = '\0', previous, *temp;
-  size_t position;
-  btree *item;
-  long point, difference;
+  char letter = '\0', // символ
+      previous,       // предыдущий символ
+      *temp;          // временная строка
+  size_t position;    // позиция
+  btree *item;        // дерево
+  long point,         // место в файле
+      difference;     // разница от point и сейчас
 
+  // в начало файла
   fseek(reader, 0, 0);
   fseek(writer, 0, 0);
 
+  // пока не конец файла
   while (!feof(reader))
   {
     previous = letter;
@@ -152,12 +170,14 @@ void replaceAllDefines(FILE *reader, FILE *writer, btree *tree)
     {
       fwrite(&letter, sizeof(char), 1, writer);
 
-      point = ftell(reader);
-      tree = handleSharp(reader, tree);
+      point = ftell(reader);            // получаем место в файле
+      tree = handleSharp(reader, tree); // обрабатываем знак решетки
 
+      // возврат на позицию (в pointer)
       difference = ftell(reader) - point;
       fseek(reader, difference * (-1), 1);
 
+      // запись символов после решетки
       temp = (char *)calloc(difference, sizeof(char));
       fread(temp, sizeof(char), difference, reader);
       fwrite(temp, sizeof(char), difference, writer);
@@ -165,18 +185,22 @@ void replaceAllDefines(FILE *reader, FILE *writer, btree *tree)
       continue;
     }
 
+    // фильтр
     if (isalpha(letter) && (previous == ' ' || previous == '(' || previous == ',' || previous == '\n'))
     {
       temp = (char *)calloc(2, sizeof(char));
       temp[0] = letter;
       position = 1;
 
+      // пока не конец файла
       while (!feof(reader))
       {
         fread(&letter, sizeof(char), 1, reader);
 
+        // запись символа к "подозрительной" последовательности символов
         temp[position] = letter;
 
+        // если фильтр сработал - остановка цикла
         if (letter == ' ' || letter == ')' || letter == ',' || letter == '\n' || letter == '(')
           break;
 
@@ -185,21 +209,21 @@ void replaceAllDefines(FILE *reader, FILE *writer, btree *tree)
       }
 
       temp[position] = '\0';
-      item = getItem(tree, temp);
+      item = getItem(tree, temp); // поиск макроса по дереву
 
-      if (item)
+      if (item) // если он есть, то заменяем его на необходимое значение
       {
         fwrite(item->value, sizeof(char), strlen(item->value), writer);
         fwrite(&letter, sizeof(char), 1, writer);
       }
-      else
+      else // иначе запишем эту последовательность символов
       {
         temp[position] = letter;
         temp[position + 1] = '\0';
         fwrite(temp, sizeof(char), strlen(temp), writer);
       }
     }
-    else
+    else // если фильтр не сработал, просто пишем этот символ
       fwrite(&letter, sizeof(char), 1, writer);
   }
 }
