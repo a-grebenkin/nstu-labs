bfc42aac1c057c65c64febd39be38f6cec8e4a6e
Aleksey-Grebenkin <61308932+Aleksey-Grebenkin@users.noreply.github.com>
2020-05-13 17:39:39 +0700

    Update Food.h

--- lab5.2/Food.h
+++ lab5.2/Food.h
@@ -3,41 +3,46 @@
 
 using namespace std;
 
+enum condition_food{
+       normal, // нормальное состояние
+       frozen, // перегрет
+       overheated // переморожен
+   };
+
 class Food
 {
-private:
-    string name;
-
-    float weight;
-
-    int temperature;
-
-    int max_temperature;
-
-    int min_temperature;
-
-    int condition; // 1 -переморожен 2 - перегрет 0 - нормальное
-
-    float heat_capacity;
-
 public:
-    Food(string name, float weight, int temperature, int max_temperature, int min_temperature, float heat_capacity);
-
+    Food(string name, double weight, double temperature, double max_temperature, double min_temperature, double heat_capacity);
     string GetName();
 
-    float GetWeight();
+    double GetWeight();
 
-    int GetTemperature();
+    double GetTemperature();
 
-    int GetMaxTemperature();
+    double GetMaxTemperature();
 
-    int GetMinTemperature();
+    double GetMinTemperature();
 
-    int GetCondition();
+    condition_food GetCondition();
 
     void TransferThermalEnergy(int Q);
 
     int GetPossibleTemperature(int Q);
 
     string GetStatus();
+
+private:
+    string name;
+
+    double weight; //kg
+
+    double temperature; //°С
+
+    double max_temperature; //°С
+
+    double min_temperature; //°С
+    
+    condition_food condition;
+    
+    double heat_capacity; //J·kg^−1·K^−1
 };
