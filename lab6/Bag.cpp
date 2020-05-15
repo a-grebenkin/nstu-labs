d86381f2ec79cf37814088961a5c19fd6f573049
Aleksey-Grebenkin <61308932+Aleksey-Grebenkin@users.noreply.github.com>
2020-05-15 20:41:34 +0700

    Update Bag.cpp

--- lab6/Bag.cpp
+++ lab6/Bag.cpp
@@ -36,19 +36,14 @@ void Bag:: PutFood(Food f){
     if (product_list.size()!=0){
         double k1=0;//k1=c1m1+c2m2+...+c(n-1)m(n-1);
         double t1=product_list[0].GetTemperature();
-        cout<<"t1= "<<t1 <<endl;
         double k2=f.GetWeight()*f.GetHeatCapacity();
-        cout<<"k2= "<<k2 <<endl;
         double t2=f.GetTemperature();
-        cout<<"t2= "<<t2 <<endl;
         for ( Food & elem : product_list )
         {
             k1+=elem.GetWeight()*elem.GetHeatCapacity();
         }
-        cout<<"k1= "<<k1 <<endl;
         product_list.push_back(f);
         double t = (k1*t1+k2*t2)/(k1+k2);
-        cout<<"t= "<<t <<endl;
         for ( Food & elem : product_list )
         {
             elem.SetTemperature(t);
