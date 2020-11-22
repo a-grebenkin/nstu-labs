df2e92beea234e7f6958a5756a57803c94a11c87
Igor Anufriev <sacmi@pm.me>
2020-11-22 11:56:53 +0700

    Visual Studio

---
+++ Forms/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace nstu_lab.Forms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
